5243e5e653f415fdf133752385a271f6d1a0eac0
Palak <palakkotwani2883@gmail.com>
2021-03-13 22:26:42 +0530

    Email Functionality added

--- Resume_/client/src/index.js
+++ Resume_/client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from "./ThemeContext";
+//import { ThemeProvider } from "./ThemeContext";
 
 ReactDOM.render(
   <React.StrictMode>
--- Resume_/server.js
+++ Resume_/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const htmlpdf = require('html-pdf');
+const cors = require('cors');
+const pdfTemplate = require('./documents');
 const path = require('path');
 const app = express();
-const pdfTemplate = require('./documents');
+var nodemailer=require('nodemailer');
 
 
 
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 //POST route is going to fetch the data and generate a PDF.
 
@@ -24,6 +27,53 @@ app.post('/create-pdf', (req, res) => {
         console.log('Success');
     });
 });
+
+
+app.post('/email',function(req,res){
+    email2=req.body.email;
+})
+
+//GET route is going to send the generated PDF to the client.
+app.get('/fetch-pdf', (req,res) => {
+    var transporter=nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:'naivecreators1303@gmail.com',
+            pass:'SHEHACKS'
+        }
+    });
+    
+    var mailOptions={
+        from:'naivecreators1303@gmail.com',
+       to: email2.toString(), //email2.toString(),
+       subject:'Here is your CV',
+        attachments: [{
+            filename: 'cv.pdf',
+            path: `${__dirname}/cv.pdf`,
+            contentType: 'application/pdf'
+          }],
+    };
+    
+    transporter.sendMail(mailOptions,function(error,info){
+        if(error)
+        console.log(error);
+        else
+        console.log('Email sent:'+ info.response);
+    });
+    
+    
+    res.sendFile(`${__dirname}/cv.pdf`);
+});
+
+//For testing the production server
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 const port = process.env.PORT || 5000;
