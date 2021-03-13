9cb420275e16db41790787e2017eb1a2a22f0068
Palak <palakkotwani2883@gmail.com>
2021-03-13 17:01:30 +0530

    Added POST Route in server.js

--- Resume_/server.js
+++ Resume_/server.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const htmlpdf = require('html-pdf');
 const path = require('path');
 const app = express();
+const pdfTemplate = require('./documents');
 
 
 
 
 app.use(cors());
+
+//POST route is going to fetch the data and generate a PDF.
+
+app.post('/create-pdf', (req, res) => {
+    htmlpdf.create(pdfTemplate(req.body), {}).toFile('cv.pdf', (err) => {
+        if(err){
+            res.send(Promise.reject());
+            console.log("MY bad");
+        }
+
+        res.send(Promise.resolve());
+        console.log('Success');
+    });
+});
 
 
 const port = process.env.PORT || 5000;
